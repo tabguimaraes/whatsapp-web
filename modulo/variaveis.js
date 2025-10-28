64a4d00f63ac3651a0679a234d08ec6f10bb9f43
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-10-28 15:29:25 -0300

    Criação do módulo de variáveis

---
+++ modulo/variaveis.js
@@ -0,0 +1,25 @@
+export const aside = {
+  message_button: document.querySelector("#message-button"),
+  user_avatar_img: document.querySelector("#user-avatar-img"),
+  user_profile_button: document.querySelector("#user-profile-button"),
+};
+
+export const contactListSection = {
+  form: document.querySelector("#form-contact-list-header"),
+  input: document.querySelector("#input-contact-list-header"),
+};
+
+export const userProfileSection = {
+  name: document.querySelector("#user-name"),
+  profile_img: document.querySelector("#profile-img"),
+  profiles_list: document.querySelector("#profiles-list"),
+  user_number: document.querySelector("#user-number"),
+};
+
+export const messageBoard = {
+  contact_avatar: document.querySelector("#contact-avatar"),
+  contact_name: document.querySelector("#contact-name"),
+  form: document.querySelector("#form-send-message"),
+  input: document.querySelector("#input-send-message"),
+  message_board_container: document.querySelector("#message-board-container"),
+};
